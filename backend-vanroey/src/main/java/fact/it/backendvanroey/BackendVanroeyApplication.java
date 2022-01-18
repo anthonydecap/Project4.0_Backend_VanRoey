fd7102f019ec74114a6c7063b93daf4f0f7185b3
Anthony Decap <r0805615@student.thomasmore.be>
2022-01-18 10:07:47 +0100

    init

---
+++ backend-vanroey/src/main/java/fact/it/backendvanroey/BackendVanroeyApplication.java
@@ -0,0 +1,13 @@
+package fact.it.backendvanroey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendVanroeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendVanroeyApplication.class, args);
+    }
+
+}
---
+++ backend-vanroey/src/test/java/fact/it/backendvanroey/BackendVanroeyApplicationTests.java
@@ -0,0 +1,13 @@
+package fact.it.backendvanroey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendVanroeyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
