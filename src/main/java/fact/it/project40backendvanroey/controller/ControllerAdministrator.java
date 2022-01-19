fb8d9c8b3f6f3d9a1cdb14781a50028bad4ec348
nemocominotto <r0797042@student.thomasmore.be>
2022-01-19 09:14:49 +0100

    Adminitstrator

--- src/main/java/fact/it/project40backendvanroey/controller/ControllerAdministrator.java
+++ src/main/java/fact/it/project40backendvanroey/controller/ControllerAdministrator.java
@@ -1,4 +1,57 @@
 package fact.it.project40backendvanroey.controller;
 
+import fact.it.project40backendvanroey.model.Administrator;
+import fact.it.project40backendvanroey.repository.AdministratorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class ControllerAdministrator {
+
+    @Autowired
+    private AdministratorRepository administratorRepository;
+
+    @GetMapping("/administrators")
+    public List<Administrator> all(){
+        return administratorRepository.findAll();
+    }
+
+    @GetMapping("/administrators/{id}")
+    public Administrator getAdminById(@PathVariable int id){
+        return administratorRepository.findAdministratorById(id);
+    }
+
+    @PostMapping("/administrators")
+    public Administrator createAdmin(@RequestBody Administrator administrator){
+        administratorRepository.save(administrator);
+
+        return administrator;
+    }
+
+    @PutMapping("/administrators")
+    public Administrator updateAdmin(@RequestBody Administrator administrator){
+        Administrator update = administratorRepository.findAdministratorById(administrator.getId());
+
+        update.setName(administrator.getName());
+        update.setLastname(administrator.getLastname());
+        update.setPassword(administrator.getPassword());
+        update.setEmail(administrator.getEmail());
+
+        administratorRepository.save(update);
+
+        return update;
+    }
+
+    @DeleteMapping("/administrators/{id}")
+    public List<Administrator> deleteAmdin(@RequestParam int id){
+        Administrator administrator = administratorRepository.findAdministratorById(id);
+
+        if(administrator!=null){
+            administratorRepository.delete(administrator);
+        }
+
+        return administratorRepository.findAll();
+    }
 }
