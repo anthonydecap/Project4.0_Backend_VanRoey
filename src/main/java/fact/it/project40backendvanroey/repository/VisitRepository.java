8de8d21ace33250bc80de732ba698a6726fb9e75
anthonydecap <r0805615@student.thomasmore.be>
2022-02-12 11:43:40 +0100

    visit order by date

--- src/main/java/fact/it/project40backendvanroey/repository/VisitRepository.java
+++ src/main/java/fact/it/project40backendvanroey/repository/VisitRepository.java
@@ -2,6 +2,7 @@ package fact.it.project40backendvanroey.repository;
 
 import fact.it.project40backendvanroey.model.Company;
 import fact.it.project40backendvanroey.model.Visit;
+import fact.it.project40backendvanroey.model.Visitor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,6 @@ import java.util.List;
 @Repository
 public interface VisitRepository extends JpaRepository<Visit, Integer> {
     Visit findVisitByVisitID(int id);
+    List<Visit> findAllByOrderByDateAsc();
     Visit findFirstByStatus(boolean status);
 }
